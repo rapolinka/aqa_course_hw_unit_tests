eb86cbcd48fc698a11178cd340c3b2ae71ad068c
prazumeika <s34086@pjwstk.edu.pl>
2025-07-26 14:58:09 +0200

    Completed task1

---
+++ task1.js
@@ -0,0 +1,30 @@
+"use strict";
+const item_1 = 5;
+console.log(item_1);
+
+const item_2 = 3;
+console.log(item_2);
+
+const item_3 = item_1 + item_2;
+console.log(item_3);
+
+const item_4 = "Hello World";
+console.log(item_4);
+
+console.log(item_3 + item_4);
+console.log(item_3 * item_4);
+
+const item_5 = item_3;
+
+const item_6 = 15;
+const item_6_type = typeof((item_6));
+console.log("iteam_6 = " + item_6 + "," + " iteam_6_type = " + item_6_type);
+
+
+
+const item_7 = String(item_6);
+const item_7_type = typeof(item_7);
+console.log("iteam_7 = " + item_7 + "," + " iteam_7_type = " + item_7_type);
+
+
+
