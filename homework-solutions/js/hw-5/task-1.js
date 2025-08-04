34e2c4ff8282f957ad158c51def3f3f0283517c8
prazumeika <s34086@pjwstk.edu.pl>
2025-08-04 16:12:30 +0200

    completed hw-5: task-1.js and task-2.js

--- homework-solutions/js/hw-5/task-1.js
+++ homework-solutions/js/hw-5/task-1.js
@@ -1,30 +1,53 @@
-/**
- * Сложить строку с четными числами от 10 до 0, разделенными `-` в переменную evenNumbersResult.
- * Переменная для результата `evenNumbersResult` уже создана и содержит пустую строку.
- * Ожидаемый результат: '10-8-6-4-2-0'
- */
+// /**
+//  * Сложить строку с четными числами от 10 до 0, разделенными `-` в переменную evenNumbersResult.
+//  * Переменная для результата `evenNumbersResult` уже создана и содержит пустую строку.
+//  * Ожидаемый результат: '10-8-6-4-2-0'
+//  */ 
 
 let evenNumbersResult = '';
 
-/**
- * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
- * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
- * Ожидаемый результат:
- * :)
- * :):)
- * :):):)
- * :):):):)
- * :):):):):)
- */
+for (let i = 10; i >= 0; i--) {
+    if (i % 2 === 0)
+        evenNumbersResult += i + "-";
+}
+evenNumbersResult = evenNumbersResult.slice(0, -1)
+
+console.log(evenNumbersResult);
+
+
+
+// /**
+//  * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
+//  * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
+//  * Ожидаемый результат:
+//  * :)
+//  * :):)
+//  * :):):)
+//  * :):):):)
+//  * :):):):):)
+//  */
+
 
 let smilePatternResult = '';
 
-/**
- * Заменить все пробелы в переменной text на "1".
- * Переменная для результата `replaceSpacesWithOneResult` уже создана и содержит пустую строку.
- * Ожидаемый результат: 'Hello!1I1am1a1JS1student!'
- */
+for (let i = 1; i <= 5; i++) {
+    smilePatternResult += ":)".repeat(i);
+    if (i < 5) smilePatternResult += '\n';
+}
+
+console.log(smilePatternResult);
+
+
+// // /**
+// //  * Заменить все пробелы в переменной text на "1".
+// //  * Переменная для результата `replaceSpacesWithOneResult` уже создана и содержит пустую строку.
+// //  * Ожидаемый результат: 'Hello!1I1am1a1JS1student!'
+// //  */
+
 const text = 'Hello! I am a JS student!';
 let replaceSpacesWithOneResult = '';
+
+replaceSpacesWithOneResult = text.replaceAll(" ", 1);
+console.log(replaceSpacesWithOneResult);
 
 export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
